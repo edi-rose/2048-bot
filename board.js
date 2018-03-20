f679dda85b1c7d9b31a261391f5cc3f92287074a
Edi Rose <edirose1998@gmail.com>
2018-03-20 21:14:33 +1300

    created board

---
+++ board.js
@@ -0,0 +1,18 @@
+var a1= {row:0, col:0, value: null}
+var a2= {row:0, col:1, value: null}
+var a3= {row:0, col:2, value: null}
+var a4= {row:0, col:3, value: null}
+var b1= {row:1, col:0, value: null}
+var b2= {row:1, col:1, value: null}
+var b3= {row:1, col:2, value: null}
+var b4= {row:1, col:3, value: null}
+var c1= {row:2, col:0, value: null}
+var c2= {row:2, col:1, value: null}
+var c3= {row:2, col:2, value: null}
+var c4= {row:2, col:3, value: null}
+var d1= {row:3, col:0, value: null}
+var d2= {row:3, col:1, value: null}
+var d3= {row:3, col:2, value: null}
+var d4= {row:3, col:3, value: null}
+
+export var board = [a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4]
