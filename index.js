784e0502f8d536394173ad46ce10b710f861a071
Edi Rose <edirose1998@gmail.com>
2018-03-21 15:04:01 +1300

    can now move values left

--- index.js
+++ index.js
@@ -14,20 +14,27 @@ function spawnNumber(){
 
 
 
-function topRowLeft(){
-  let topRow = board.filter(square => square.row == 0).reverse()
-  topRow.map((square, idx, array)=>{
-    if(square.value == null){
-      return
-    }
-    if(array[idx+1] == undefined){
-      return
-    }
-      if(array[idx+1].value == null){
-        array[idx+1].value = square.value
-        square.value = null
+function moveValuesLeft(){
+  let firstRow = board.filter(square => square.row == 0).reverse()
+  let secondRow = board.filter(square => square.row == 1).reverse()
+  let thirdRow = board.filter(square => square.row == 2).reverse()
+  let forthRow = board.filter(square => square.row == 3).reverse()
+  let allRows = [firstRow, secondRow, thirdRow, forthRow]
+  allRows.map(row => {
+    row.map((square, idx, array)=>{
+      if(square.value == null){
+        return
       }
+      if(array[idx+1] == undefined){
+        return
+      }
+        if(array[idx+1].value == null){
+          array[idx+1].value = square.value
+          square.value = null
+        }
+    })
   })
 }
-topRowLeft()
+
+moveValuesLeft()
 console.log(board)
